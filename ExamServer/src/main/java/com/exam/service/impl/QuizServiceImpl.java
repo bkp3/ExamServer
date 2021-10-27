f57b967a6fc63148ab49e4a57bd7d746f5f60c41
Bhola Kumar <bholakumar.7372@gmail.com>
2021-10-27 07:47:12 +0530

    deleting quiz

--- ExamServer/src/main/java/com/exam/service/impl/QuizServiceImpl.java
+++ ExamServer/src/main/java/com/exam/service/impl/QuizServiceImpl.java
@@ -40,9 +40,8 @@ public class QuizServiceImpl implements QuizService {
 
 	@Override
 	public void deleteQuiz(Long quizId) {
-		Quiz quiz = new Quiz();
-		quiz.setqId(quizId);
-		this.quizRepository.delete(quiz);
+
+		this.quizRepository.deleteById(quizId);
 	}
 
 }
